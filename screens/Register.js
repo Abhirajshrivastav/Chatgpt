41da420f1091ab472870a11246021315c766cd89
Abhirajshrivastav <abhiraj1234sri1234@gmail.com>
2023-08-03 11:31:39 +0530

    Firebase intigrated

--- screens/Register.js
+++ screens/Register.js
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
-import React, { useCallback, useReducer } from 'react'
+import { View, Text, Image, Alert } from 'react-native'
+import React, { useCallback, useEffect, useReducer, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { COLORS, FONTS, SIZES, images } from '../constants'
 import PageContainer from '../components/PageContainer'
-import { Image } from 'react-native'
 import Input from '../components/Input';
 import Button from '../components/Button'
 import { reducer } from "../utils/reducers/formReducer";
 import { validateInput } from '../utils/actions/formActions'
+import { getFirebaseApp } from '../utils/firebaseHelper'
+import {getAuth, createUserWithEmailandPassword} from "firebase/auth"
+import { ref , child , set , getDatabase} from "firebase/database"
+import { isLoading } from 'expo-font'
 
 const initialState = {
   inputValus: {
@@ -23,14 +26,79 @@ const initialState = {
   formIsValid: false,
 }
 
-const Register = () => {
+const Register = ({navigation}) => {
 
-  const[formState, dispatchFormState] = useReducer(reducer, initialState);
+  const [formState, dispatchFormState] = useReducer(reducer, initialState)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  const inputChangeHandler = useCallback((inputId,inputValue) => {
-    const result = validateInput(inputId, inputValue);
-    dispatchFormState({inputId, validationResult: result, inputValue})
-  }, [dispatchFormState])
+  const inputChangedHandler = useCallback(
+    (inputId, inputValue) => {
+        const result = validateInput(inputId, inputValue)
+        dispatchFormState({ inputId, validationResult: result, inputValue })
+    },
+    [dispatchFormState]
+)
+
+ 
+  const createUser = async (fullName, email, userId) => {
+    const userData = {
+        fullName,
+        email,
+        userId,
+        signUpDate: new Date().toISOString(),
+    }
+
+    const dbRef = ref(getDatabase())
+    const childRef = child(dbRef, `users/${userId}`)
+    await set(childRef, userData)
+
+    return userData
+}
+
+
+const authHandler = async () => {
+  const app = getFirebaseApp()
+  const auth = getAuth(app)
+  setIsLoading(true)
+
+  try {
+      const result = await createUserWithEmailandPassword(
+          auth,
+          formState.inputValues.email,
+          formState.inputValues.password
+      )
+
+      const { uid } = result.user
+
+      const userData = await createUser(
+          formState.inputValues.fullName,
+          formState.inputValues.email,
+          uid
+      )
+
+      if (userData) {
+          setIsLoading(false)
+          navigation.navigate('Login')
+      }
+  } catch (error) {
+      const errorCode = error.code
+      let message = 'Something went wrong !'
+      if (errorCode === 'auth/email-already-in-use') {
+          message = 'This email is already in use'
+      }
+
+      setError(message)
+      setIsLoading(false)
+  }
+}
+
+  // Display error if any error occured 
+  useEffect(() => {
+    if (error) {
+        Alert.alert('An error occured', error)
+    }
+}, [error])
 
   return (
    <SafeAreaView 
@@ -58,34 +126,37 @@ const Register = () => {
           Welcome back
         </Text>
         <Input 
-        inInputChanged={inputChangeHandler}
+           onInputChanged={inputChangedHandler}
         errorText={formState.inputValidities['fullName']}
       id="fullName"
       placeholder="Enter your full name"
       placeholderTextColor={COLORS.text}
         />
          <Input 
-          inInputChanged={inputChangeHandler}
+              onInputChanged={inputChangedHandler}
           errorText={formState.inputValidities['email']}
       id="email"
       placeholder="Enter your email"
       placeholderTextColor={COLORS.text}
         />
         <Input 
-        inInputChanged={inputChangeHandler}
-        errorText={formState.inputValidities['password']}
-       id="password"
+          onInputChanged={inputChangedHandler}
+          errorText={formState.inputValidities['password']}
+          id="password"
        placeholder="Enter your password"
        placeholderTextColor={COLORS.text}
+       secureTextEntry
         />
          <Button
             title="Register"
+            onPress={authHandler}
+            isLoading={isLoading}
             filled
-             style={{
+            style={{
                 width: SIZES.width - 44,
                 marginBottom: SIZES.padding,
-                 marginVertical: 8,
-             }}
+                marginVertical: 8,
+              }}
                     />  
   </View>
 </PageContainer>
---
+++ utils/firebaseHelper.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export const getfirebaseApp = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBIWeX6oC7WZsgzrCqYMxkJUeiNvLUcJL8",
+        authDomain: "chatgpt-reactjs-native.firebaseapp.com",
+        projectId: "chatgpt-reactjs-native",
+        storageBucket: "chatgpt-reactjs-native.appspot.com",
+        messagingSenderId: "914275074287",
+        appId: "1:914275074287:web:c67ccd06ff8e3f88bedb1a"
+      };
+      
+      // Initialize Firebase
+      return initializeApp(firebaseConfig);
+}
+
+
+// Your web app's Firebase configuration
